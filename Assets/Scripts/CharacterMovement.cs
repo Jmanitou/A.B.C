992e441968b500a5e1283b1b7deb4a1b042a4aba
Rudy Zhang <rxz2801@rit.edu>
2019-10-02 13:00:17 -0400

    Spritesheet loaded. Character can now move horiztontally
    Basic movements implemented, not finished

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontally();
+    }
+
+    // Control character's horizontal movement
+    void MoveHorizontally()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position += horizontal * speed * Time.deltaTime;
+    }
+}
