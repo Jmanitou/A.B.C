1cc7a2afaf9f76f8ddd553f7f928a6004c7c45da
Rudy Zhang <rxz2801@rit.edu>
2019-10-06 00:55:45 -0400

    Added timer for stealing

---
+++ Assets/Scripts/GuardMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Ignore the collision between the guard and the player
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Colliding with player");
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
+}
--- Assets/Scripts/Stealing.cs
+++ Assets/Scripts/Stealing.cs
@@ -1,34 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stealing : MonoBehaviour
 {
-    [SerializeField] private GameObject[] goalItems;
+    [SerializeField] private List<GameObject> goalItems;
     public Transform playerTransform;
 
-    [SerializeField] private float range;   // Range for the player to interact (steal) with the museum collection
+    [SerializeField] private float timeToSteal = 3.0f;
+    [SerializeField] private float stealTimer = 0f;
+    public Slider playerStealSlider;
+
+    [SerializeField] 
+    [Range(0f, 3f)]
+    private float range = 0;   // Range for the player to interact (steal) with the museum collection
+
+    private void Awake()
+    {
+        goalItems = new List<GameObject>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        goalItems = GameObject.FindGameObjectsWithTag("GoalItem");
+        GameObject[] temps = GameObject.FindGameObjectsWithTag("GoalItem");
+        foreach (GameObject temp in temps)
+        {
+            goalItems.Add(temp);
+        }
+
+        // Set slider
+        playerStealSlider.maxValue = timeToSteal;
+        playerStealSlider.value = stealTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U))
+        if (Input.GetKey(KeyCode.U))
         {
-            Steal(FindClosestGoalItem());
+            stealTimer += Time.deltaTime; // Time accumulating
+
+            if (stealTimer >= timeToSteal)
+            {
+                Steal(FindClosestGoalItem());
+                stealTimer = 0f;
+            }
         }
     }
 
     // Steal a goal item given the index in the goalItems array
     void Steal(int index)
     {
-        // Disable the child (museum collection) of a goal item
-        goalItems[index].transform.GetChild(0).gameObject.SetActive(false);
+        // If the player is within range
+        if (Vector2.Distance(playerTransform.position, goalItems[index].transform.position) <= range)
+        {
+            // Disable the child (museum collection) of a goal item
+            goalItems[index].transform.GetChild(0).gameObject.SetActive(false);
+
+            // Remove the item from the list since it has been stolen
+            goalItems.RemoveAt(index);
+        }
     }
 
     int FindClosestGoalItem()
@@ -38,7 +71,7 @@ public class Stealing : MonoBehaviour
         int closestIndex = 0;
         float closestDistance = float.MaxValue;
 
-        for (int i = 0; i < goalItems.Length; ++i)
+        for (int i = 0; i < goalItems.Count; ++i)
         {
             GameObject go = goalItems[i];
 
@@ -53,5 +86,15 @@ public class Stealing : MonoBehaviour
         }
 
         return closestIndex;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw the range of the object
+        Gizmos.color = Color.yellow;
+        foreach (GameObject go in goalItems)
+        {
+            Gizmos.DrawWireSphere(go.transform.position, range);
+        }
     }
 }
