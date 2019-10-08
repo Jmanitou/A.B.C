bcf5bfca64749115b91bb134e9fccdde551193f1
Rudy Zhang <rxz2801@rit.edu>
2019-10-08 03:07:42 -0400

    Added basic patrol behavior of the guard ai

--- Assets/Scripts/GuardAI.cs
+++ Assets/Scripts/GuardAI.cs
@@ -11,19 +11,31 @@ enum StateType
 
 public class GuardAI : MonoBehaviour
 {
+    // General Variables
     [SerializeField] private StateType currentState = 0;
+    private Vector2 spawnPosition;
+    public GameObject flashLight;
+
+    // Patrol Movement Variables
+    [SerializeField] [Range(0f, 10f)] private float guardPatrolSpeed = 5f;
+    [SerializeField] bool isGoingLeft = false;
+    [SerializeField] [Range(0f, 50f)] float patrolDist = 30.0f;     // Max distance for patrolling
 
     // Start is called before the first frame update
     void Start()
     {
         // Initialize guard's state
         currentState = StateType.Patrol;
+
+        spawnPosition = transform.position;
+        isGoingLeft = true;
+        FlipSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateState(currentState);
     }
 
     // Implementation of a state FSM
@@ -32,6 +44,7 @@ public class GuardAI : MonoBehaviour
         switch (currentState)
         {
             case StateType.Patrol:
+                Patrol();
                 break;
             case StateType.Chase:
                 break;
@@ -51,6 +64,37 @@ public class GuardAI : MonoBehaviour
     }
 
     #region Helper Methods
+
+    void Patrol()
+    {
+        float distFromSpawn = transform.position.x - spawnPosition.x;
+
+        // Gone too far, switch patrol direction
+        if (Mathf.Abs(distFromSpawn) >= patrolDist)
+            SwitchDirection();
+
+        if (isGoingLeft)
+        {
+            // Move leftward
+            transform.Translate(-guardPatrolSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            // Move rightward
+            transform.Translate(guardPatrolSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    // Switch the movement direction
+    void SwitchDirection()
+    {
+        isGoingLeft = !isGoingLeft;
+        FlipSprite();
+        FlipLight();
+    }
+
+    void FlipSprite() { GetComponent<SpriteRenderer>().flipX = !isGoingLeft ? true : false; }
+    void FlipLight() { flashLight.transform.Rotate(new Vector3(0f, 180f, 0f), Space.World); }
 
     #endregion
 }
