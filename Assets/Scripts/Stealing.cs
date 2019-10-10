85a82ffb8fd076fad390d77f5b32966befe7d315
Jmanitou <Gumsterrocks@gmail.com>
2019-10-10 09:06:13 -0400

    Dematerilize and Stealing Timer fixed
    Fixed flickering of dematerilizing
    Made stealing timer reset when not stealing an object

--- Assets/Scripts/Stealing.cs
+++ Assets/Scripts/Stealing.cs
@@ -70,6 +70,8 @@ public class Stealing : MonoBehaviour
                 {
                     // If not holding stealing key, hide the slider
                     playerStealSlider.gameObject.SetActive(false);
+                    stealTimer = 0f;
+                    playerStealSlider.value = 0f;
                 }
             }
 
