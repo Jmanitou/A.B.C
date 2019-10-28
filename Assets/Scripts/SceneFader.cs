e8879a4ff6173e7a3b68a740c99453e9ab7d43d1
Rudy Zhang <rxz2801@rit.edu>
2019-10-28 01:28:09 -0400

    Updated scene fader and start menu

---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    /// <summary>
+    /// Fade to a certain scene in game
+    /// </summary>
+    /// <param name="scene">Scene to be transitioned to</param>
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        while (t > 0f) // Keep animating until t reaches 0
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; // Skip to the next frame
+        }
+    }
+
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0f;
+
+        while (t < 1f) // Keep animating until t reaches 0
+        {
+            t += Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; // Skip to the next frame
+        }
+
+        // Load the scene
+        SceneManager.LoadScene(scene);
+    }
+}
